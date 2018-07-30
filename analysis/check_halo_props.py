2eac16bf434e2c6471e2f74a6d2def0a4152faa3
Ekta Patel <ektapatel@email.arizona.edu>
2018-07-30 10:52:03 -0700

    new proposal for phast analog analysis and moved halo props analysis script

--- analysis/check_halo_props.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-data = np.loadtxt('../data/M31analogs_halo_props.txt')
-mmt = data[:,9]
-print mmt
-
-print len(mmt)
-print len(mmt[(mmt >=1.)*(mmt <=4.)])
-print len(mmt[(mmt >= 8.)]), mmt[mmt >= 8.]
---
+++ phast/check_halo_props.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+data = np.loadtxt('../data/M31analogs_halo_props.txt')
+mmt = data[:,9]
+mstar = data[:,10]/0.704*1e10
+
+print np.min(mstar), np.max(mstar)
+
+print len(mmt)
+print len(mmt[(mmt >=1.)*(mmt <=4.)*(mstar >= 5e10)*(mstar <= 2e11)])
+print len(mmt[(mmt >= 8.)*(mstar >= 5e10)*(mstar <= 2e11)])
