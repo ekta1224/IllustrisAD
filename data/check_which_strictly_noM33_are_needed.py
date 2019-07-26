e7fc11bc2b9639ac2c6aaf3ecfdc378d9c11acf0
EKTA PATEL <ektapatel@email.arizona.edu>
2019-07-25 17:06:36 -0700

    added files for M31 analogs without M33 to a Google Drive folder

--- data/check_which_strictly_noM33_are_needed.py
+++ data/check_which_strictly_noM33_are_needed.py
@@ -2,19 +2,19 @@ import numpy as np
 import os
 from os import path
 
-ids = np.loadtxt('M31analogs_halo_props_strictly_noM33.txt')
+ids = np.loadtxt('M31analogs_halo_props_strictly_noM33.txt')[:,0]
 print(len(ids))
 need_data = []
 have_data = []
 for i in ids:
+    print(i)
     res = (str(path.exists('M31analog_%s_gas_properties.txt'%int(i))))
     print (res)
     if res =='False':
         need_data.append(i)
     else:
          have_data.append(i)
-#         os.system('git add M31analog_%s_gas_properties.txt'%int(i))
-#         os.system('git add M31analog_%s_star_properties.txt'%int(i))
+         os.system('cp M31analog_%s_star_properties.txt ./M31analogs_strictly_noM33_data'%int(i))
 print(need_data)
 print(len(need_data))
 
