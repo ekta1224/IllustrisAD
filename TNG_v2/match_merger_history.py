8f22204576abaed63df9452f6146f087710a6b6a
AmandaQuirk <amandaquirk@comcast.net>
2020-04-10 16:31:21 -0400

    added header to output file

--- TNG_v2/match_merger_history.py
+++ TNG_v2/match_merger_history.py
@@ -6,7 +6,7 @@ revised_ids = np.loadtxt('/Volumes/Titan/analogs/IllustrisAD/TNG_v2/M31analogs_h
 revised_ids = [int(a) for a in revised_ids]
 
 #read in the merger file
-merger_data = h5py.File('merger_history_099.hdf5', 'r')
+merger_data = h5py.File('/Volumes/Titan/analogs/TNGdata/merger_history_099.hdf5', 'r')
 
 #pull out the params we want: time of last major merger, number of major mergers, and number of minor mergers and match the data to our analogs
 last_major_time = merger_data['SnapNumLastMajorMerger'][revised_ids]
@@ -14,5 +14,5 @@ num_major = merger_data['NumMajorMergersTotal'][revised_ids]
 num_minor = merger_data['NumMinorMergersTotal'][revised_ids]
 
 #save file
-np.savetxt('/Volumes/Titan/analogs/TNGdata/M31analogs_merger_props_TNG100_revised.txt', np.c_[revised_ids, last_major_time, num_major, num_minor], header='snapshot number of last major merger, number of total major mergers, number of total minor mergers')
+np.savetxt('/Volumes/Titan/analogs/TNGdata/M31analogs_merger_props_TNG100_revised.txt', np.c_[revised_ids, last_major_time, num_major, num_minor], header='analog ID, snapshot number of last major merger, number of total major mergers, number of total minor mergers')
 
